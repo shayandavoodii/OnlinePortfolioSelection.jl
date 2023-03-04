8ec959b929f4cc66bf466557f2461a9f6183745f
Shayan <sh0davoodi@gmail.com>
2023-03-04 23:51:08 +0330

    RPRT edited

---
+++ src/Tools/tools.jl
@@ -0,0 +1,22 @@
+function simplex_proj(b::Vector)
+  n_assets = length(b)
+  cond = false
+
+  sorted_b = sort(b, rev=true)
+  tmpsum = 0.
+
+  for i in 1:n_assets-1
+    tmpsum += sorted_b[i]
+    tmax = (tmpsum - 1.)/i
+    if tmax≥sorted_b[i]
+      cond = true
+      break
+    end
+  end
+
+  if !cond
+    tmax = (tmpsum + sorted_b[n_assets] - 1.)/n_assets
+  end
+
+  max.(b .- tmax, 0.)
+end;

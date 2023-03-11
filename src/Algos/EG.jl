88c2cae1e82ccaad75b78fb106144cc10614f41c
Shayan <sh0davoodi@gmail.com>
2023-03-11 21:39:30 +0330

    Added documentation for `EG`.

--- src/Algos/EG.jl
+++ src/Algos/EG.jl
@@ -4,6 +4,26 @@ struct EG
   budgets::Vector{Float64}
 end;
 
+"""
+    EG(adj_close::Matrix{Float64}, initial_budget=1, eta=0.05)
+
+Exponential Gradient (EG) algorithm.
+
+Calculate the Exponential Gradient (EG) weights and budgets
+using the given historical prices and parameters and return
+an EG object.
+
+# Arguments
+- `adj_close::Matrix{Float64}`: Historical adjusted close prices.
+- `initial_budget=1.`: Initial budget.
+- `eta=0.05`: Learning rate.
+
+# Returns
+- `::EG(n_assets, weights, budgets)`: Exponential Gradient (EG) object.
+
+# References
+- [1] [On-Line Portfolio Selection Using Multiplicative Updates](https://doi.org/10.1111/1467-9965.00058)
+"""
 function EG(adj_close::Matrix{Float64}, initial_budget=1, eta=0.05)
   # Calculate relative prices
   @views relative_prices = adj_close[:, 2:end] ./ adj_close[:, 1:end-1]

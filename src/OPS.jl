10a9330f6274521ba7f724d255f01852a807cdff
Shayan <sh0davoodi@gmail.com>
2023-04-20 11:59:22 +0430

    Metrics got exported. 🚚

--- src/OPS.jl
+++ src/OPS.jl
@@ -6,8 +6,10 @@ include("Algos/RPRT.jl")
 include("Algos/UP.jl")
 include("Algos/CORN.jl")
 include("Algos/DRICORNK.jl")
+include("Tools/metrics.jl")
 
 export UP, EG, RPRT, UP, CORNU, CORNK, DRICORNK
+export OPSMetrics, Sn, APY, MDD, Calmar, Ann_Sharpe
 
 
 end #module

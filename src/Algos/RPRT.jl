12250bdeeeda214a80acb0019c3ed8e7807846e5
Shayan <sh0davoodi@gmail.com>
2023-04-24 00:06:58 +0430

    Code refactoring 🛠

--- src/Algos/RPRT.jl
+++ src/Algos/RPRT.jl
@@ -4,17 +4,19 @@ include("../Tools/tools.jl");
 
 struct RPRT
   n_assets::Int
-  weights::Matrix{Float64}
+  b::Matrix{Float64}
   budgets::Vector{Float64}
 end;
 
 function RPRT(
-  adj_close::Matrix{Float64};
+  adj_close::Matrix{T};
   w::Int64=5,
   initial_budget::Int=1,
-  theta::Float64=0.8,
-  epsilon=50)
-  @assert w≥2 "Window length (w) must be greater than 1"
+  theta::T=0.8,
+  epsilon=50
+) where T<:Float64
+
+  w≥2 || ArgumentError("Window length (w) must be greater than 1") |> throw
 
   n_assets, n_periods = size(adj_close)
   relative_prices = adj_close[:, 2:end] ./ adj_close[:, 1:end-1]
@@ -25,7 +27,7 @@ function RPRT(
   b = zeros(n_assets, n_periods)
   last_b = ones(n_assets)/n_assets
 
-  for t in axes(adj_close, 2)
+  for t ∈ axes(adj_close, 2)
 
     if t<w
       b[:, t] = last_b
@@ -37,7 +39,7 @@ function RPRT(
     prediction = predict_relative_price(relative_prices[:, t-w+1:t-1])
 
     # predicted d
-    dₚ = diagm(vec(prediction))
+    dₚ = vec(prediction) |> diagm
 
     # predicted γ
     γₚ = θ * last_relative_price ./ (θ*last_relative_price+ϕ)
@@ -70,7 +72,7 @@ function RPRT(
   b = b./sum(b, dims=1)
   budgets = ones(n_periods)*initial_budget
 
-  for t in axes(adj_close, 2)[1:end-1]
+  for t ∈ axes(adj_close, 2)[1:end-1]
     budgets[t+1] = budgets[t] * sum(relative_prices[:, t] .* b[:, t])
   end
 

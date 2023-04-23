595d0a6d7d73a29191ffb5bd3ccf2d6cd200f128
Shayan <sh0davoodi@gmail.com>
2023-04-24 00:06:43 +0430

    Code refactoring 🛠

--- src/Algos/EG.jl
+++ src/Algos/EG.jl
@@ -1,6 +1,6 @@
 struct EG
   n_asset::Int
-  weights::Matrix{Float64}
+  b::Matrix{Float64}
   budgets::Vector{Float64}
 end;
 
@@ -28,14 +28,13 @@ function EG(adj_close::Matrix{Float64}, initial_budget=1, eta=0.05)
   # Calculate relative prices
   @views relative_prices = adj_close[:, 2:end] ./ adj_close[:, 1:end-1]
 
-  n_assets = size(adj_close, 1)
-  n_periods = size(adj_close, 2)
+  n_assets, n_periods = size(adj_close)
 
   # Initialiate Vector of weights
   b = fill(1/n_assets, (n_assets, n_periods))
 
   # Calculate weights
-  for t in axes(adj_close, 2)[2:end]
+  for t ∈ axes(adj_close, 2)[2:end]
     prev_b = @view b[:, t-1]
     @views w = prev_b .* exp.(
       eta.*relative_prices[:, t-1]/sum(relative_prices[:, t-1].*b[:, t-1])
@@ -48,7 +47,7 @@ function EG(adj_close::Matrix{Float64}, initial_budget=1, eta=0.05)
   budget = zeros(n_periods)
   budget[1] = initial_budget
 
-  for t in 1:n_periods-1
+  for t ∈ 1:n_periods-1
     budget[t+1] = budget[t] * sum(relative_prices[:, t] .* b[:, t])
   end
 

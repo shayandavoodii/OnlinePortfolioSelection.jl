8f5d69c4c8fa5b36a5de74d84caec8acee1308f5
Shayan <sh0davoodi@gmail.com>
2023-04-24 00:06:22 +0430

    Code refactoring 🛠

--- src/Algos/CRP.jl
+++ src/Algos/CRP.jl
@@ -8,8 +8,7 @@ function CRP(adj_close::Matrix{Float64}, initial_budget=1)
   # Calculate relative prices
   @views relative_prices = adj_close[:, 2:end] ./ adj_close[:, 1:end-1]
 
-  n_assets = size(adj_close, 1)
-  n_periods = size(adj_close, 2)
+  n_assets, n_periods = size(adj_close)
 
   # Calculate b (initial weights)
   b = fill(1/n_assets, (n_assets, n_periods))

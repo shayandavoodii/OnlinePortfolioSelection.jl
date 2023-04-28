95730e9d0186e02130a476c003bb9e48a34bbe10
Shayan <sh0davoodi@gmail.com>
2023-04-28 13:50:56 +0430

    `invest` function removed.

--- src/Tools/tools.jl
+++ src/Tools/tools.jl
@@ -1,5 +1,3 @@
-const METHODS1 = (:CORN, :DRICORN, :DRICORNK)
-
 function simplex_proj(b::Vector)
   n_assets = length(b)
   cond = false
@@ -23,55 +21,6 @@ function mc_simplex(d, points)
   a = sort(rand(points, d), dims=2)
   a = [zeros(points) a ones(points)]
   diff(a, dims=2)
-end
-
-"""
-    invest(
-      method::Symbol,
-      horizon::Int,
-      weights::Matrix{T},
-      reltv_pr::Matrix{T},
-      initial_budget
-    ) where T<:Float64
-
-  Invest the budget in the assets according to the given weights in the given horizon.
-
-  # Arguments
-  - `method::Symbol`: The method that is used for investment.
-  - `horizon::Int`: The number of periods that the investment is made.
-  - `weights::Matrix{T}`: The weights of the assets in each period.
-  - `reltv_pr::Matrix{T}`: The relative prices of the assets in each period.
-  - `initial_budget`: The initial budget.
-
-  # Returns
-  - `budgets::Vector{T}`: The budget during the investment horizon.
-"""
-function invest(
-  method::Symbol,
-  horizon::Int,
-  weights::Matrix{T},
-  reltv_pr::Matrix{T},
-  initial_budget
-) where T<:Float64
-
-  if method ∈ METHODS1
-    return budg_dur_time(horizon, weights, reltv_pr, initial_budget)
-  end
-end
-
-function budg_dur_time(
-  horizon::Int,
-  weights::Matrix{T},
-  reltv_pr::Matrix{T},
-  initial_budget
-) where T<:Float64
-  # Calculate budgets
-  budgets = zeros(T, horizon+1)
-  budgets[1] = initial_budget
-  for t in 1:horizon
-    @views budgets[t+1] = budgets[t] * sum(reltv_pr[:, end-horizon+t] .* weights[:, t])
-  end
-  return budgets
 end
 
 """

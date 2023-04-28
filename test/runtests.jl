372dd7fdda521f6e4eda0bc45abda8c700d7a958
Shayan <sh0davoodi@gmail.com>
2023-04-28 13:45:02 +0430

    Unit tests for `UP`, `RPRT`, `EG`, and `CRP` provided.

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,3 +20,23 @@ end
   @info "Run unit tests in metrics.jl"
   include("metrics.jl")
 end
+
+@testset "UP.jl" begin
+  @info "Run unit tests in UP.jl"
+  include("UP.jl")
+end
+
+@testset "RPRT.jl" begin
+  @info "Run unit tests in RPRT.jl"
+  include("RPRT.jl")
+end
+
+@testset "EG.jl" begin
+  @info "Run unit tests in EG.jl"
+  include("EG.jl")
+end
+
+@testset "CRP.jl" begin
+  @info "Run unit tests in CRP.jl"
+  include("CRP.jl")
+end

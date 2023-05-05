31c3fd0a086918787e18399ec5d93b0bcc63c218
Shayan <sh0davoodi@gmail.com>
2023-05-05 10:06:24 +0430

    Prevent RPRT from being excluded.

--- src/OnlinePortfolioSelection.jl
+++ src/OnlinePortfolioSelection.jl
@@ -2,7 +2,7 @@ module OnlinePortfolioSelection
 
 include("Algos/CRP.jl")
 include("Algos/EG.jl")
-include("Algos/RPRT.jl")
+# include("Algos/RPRT.jl")
 include("Algos/UP.jl")
 include("Algos/CORN.jl")
 include("Algos/DRICORNK.jl")

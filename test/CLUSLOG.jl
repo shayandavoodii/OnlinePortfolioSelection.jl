05c17b684be1d0037a48436b98b2e6bd0e37a418
Shayan <sh0davoodi@gmail.com>
2023-10-27 11:18:46 +0330

    Further unit tests provided 🧪

--- test/CLUSLOG.jl
+++ test/CLUSLOG.jl
@@ -6,7 +6,14 @@ adj_close = [
   1.3212 1.3315 1.3213 1.3153 1.3031 1.2913 1.2950 1.2953 1.3315 1.3213 1.3315
 ]
 
+adj_close2 = [
+  1.5464 1.5852 1.6532 1.7245 1.5251 10.4185 2.2156 0.3231 1.3585 1.4563 1.4456
+  1.2411 1.2854 1.3456 1.4123 1.5212 8.5015 1.4913 0.5212 1.5015 1.4913 1.5015
+  1.3212 1.3315 1.3213 1.3153 1.3031 3.2913 7.2950 15.2953 1.3315 1.3213 1.3315
+]
+
 rel_pr = adj_close[:, 2:end]./adj_close[:, 1:end-1]
+rel_pr2 = adj_close2[:, 2:end]./adj_close2[:, 1:end-1]
 nassets = size(rel_pr, 1)
 horizon = 3
 TW = 3
@@ -25,6 +32,13 @@ lb, ub = 0., 1.
     @test model.alg == "KMDLOG"
     @test sum(model.b, dims=1) .|> isapprox(1.) |> all
     @test size(model.b) == (nassets, horizon)
+
+    # Intended for testing the algorithm if there is no similar time windows found at the first day
+    model = cluslog(rel_pr2, horizon, TW, KmedoidsModel, nclusters_, nclustering, (lb, ub))
+    @test model.alg == "KMDLOG"
+    @test sum(model.b, dims=1) .|> isapprox(1.) |> all
+    @test size(model.b) == (nassets, horizon)
+
   end
 
   @testset "With invalid args" begin

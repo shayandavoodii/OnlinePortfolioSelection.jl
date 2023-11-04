4ede721eeffb29007ee67da2c5e3eb65496dfba3
Shayan <sh0davoodi@gmail.com>
2023-11-04 22:34:51 +0330

    same as cb15651aa7c031fe5901af22170307c4872d5905, doc added ➕

--- src/Types/Clustering.jl
+++ src/Types/Clustering.jl
@@ -1,8 +1,31 @@
 abstract type ClusteringModel end
 
-@kwdef struct KmeansModel<:ClusteringModel
+"""
+    KMNModel(alg::String="KMNLOG")<:ClusteringModel
+
+`KMNModel` is a concrete type used to represent the KMNLOG Model.
+
+# Fields
+- `alg::String="KMNLOG"`: The algorithm's name to be used.
+
+!!! warning
+    Do not try to change the value of `alg` field.
+"""
+@kwdef struct KMNModel<:ClusteringModel
   alg::String="KMNLOG"
 end
-@kwdef struct KmedoidsModel<:ClusteringModel
+
+"""
+    KMDModel(alg::String="KMDLOG")<:ClusteringModel
+
+`KMDModel` is a concrete type used to represent the KMDLOG Model.
+
+# Fields
+- `alg::String="KMDLOG"`: The algorithm's name to be used.
+
+!!! warning
+    Do not try to change the value of `alg` field.
+"""
+@kwdef struct KMDModel<:ClusteringModel
   alg::String="KMDLOG"
 end

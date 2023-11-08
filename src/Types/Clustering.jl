8d09487f2c0acf5b7518c2b49f3c20d922ca0391
Shayan <sh0davoodi@gmail.com>
2023-11-09 00:18:09 +0330

    Doc fixed in Clustering.jl. 🔧

--- src/Types/Clustering.jl
+++ src/Types/Clustering.jl
@@ -1,14 +1,14 @@
 abstract type ClusLogVariant end
 
 """
-    KMNLOG(alg::String="KMNLOG")<:ClusLogVariant
+    KMNLOG<:ClusLogVariant
 
 `KMNLOG` is a concrete type used to represent the KMNLOG Model. Also, see [`KMDLOG`](@ref).
 """
 struct KMNLOG<:ClusLogVariant end
 
 """
-    KMDLOG(alg::String="KMDLOG")<:ClusLogVariant
+    KMDLOG<:ClusLogVariant
 
 `KMDLOG` is a concrete type used to represent the KMDLOG Model. Also, see [`KMNLOG`](@ref).
 """

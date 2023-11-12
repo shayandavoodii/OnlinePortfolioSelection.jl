5855b2a17a06bb0e1c7d37a6a210b9b6c48d3275
Shayan <sh0davoodi@gmail.com>
2023-11-12 20:00:23 +0330

    BAH(OLMAR) more unit tests provided 🧪 (desc) + Overall unit test justifications 🔧

--- test/OLMAR.jl
+++ test/OLMAR.jl
@@ -2,29 +2,44 @@ adj1 = [
   1.315 1.326 1.358 1.39461 1.424 1.4015 1.52531
   1.215 1.111 1.215 1.35614 1.454 1.2158 1.32561
 ]
-adj2 = rand(3, 20);
+
+adj2 = [
+  1.315 1.326 1.358 1.39461 1.424 1.4015 1.52531 1.375 1.396 1.378 1.39161 1.454 1.4315 1.57531
+  1.215 1.111 1.215 1.35614 1.454 1.2158 1.32561 1.295 1.121 1.255 1.34614 1.404 1.1158 1.30561
+];
+
 n_assets_adj1 = size(adj1, 1)
 n_assets_adj2 = size(adj2, 1)
 horizon = 3
 
 @testset "OLMAR.jl" begin
-  @testset "All good" begin
-    model = olmar(adj1, horizon, 3, 2)
+  @testset "With valid arguments" begin
+    @testset "OLMAR" begin
+      model = olmar(adj1, horizon, 3, 2)
+      @test all(sum(model.b, dims=1) .≈ 1.0)
+      @test size(model.b) == (n_assets_adj1, horizon)
+      @test model.n_assets == n_assets_adj1
 
-    @test all(sum(model.b, dims=1) .≈ 1.0)
+      model = olmar(adj2, horizon, 3, 2)
+      @test all(sum(model.b, dims=1) .≈ 1.0)
+      @test size(model.b) == (n_assets_adj2, horizon)
+      @test model.n_assets == n_assets_adj2
+    end
 
-    @test size(model.b) == (n_assets_adj1, horizon)
-
-    model = olmar(adj2, horizon, 3, 2)
-
-    @test all(sum(model.b, dims=1) .≈ 1.0)
-
-    @test size(model.b) == (n_assets_adj2, horizon)
+    @testset "BAH(OLMAR)" begin
+      model = olmar(adj2, horizon, [3, 5, 4], 2)
+      @test all(sum(model.b, dims=1) .≈ 1.0)
+      @test size(model.b) == (n_assets_adj2, horizon)
+      @test model.n_assets == n_assets_adj2
+    end
   end
-
-  @testset "Errors" begin
+  @testset "With invalide arguments" begin
     @test_throws ArgumentError olmar(adj1, horizon, 3, 1)
     @test_throws ArgumentError olmar(adj1, horizon, 2, 2)
     @test_throws ArgumentError olmar(adj1, 6, 3, 2)
+    @test_throws ArgumentError olmar(adj2, 3, [3], 2)
+    @test_throws ArgumentError olmar(adj2, 3, [3, 2], 2)
+    @test_throws ArgumentError olmar(adj2, 3, [3, 13], 2)
+    @test_throws ArgumentError olmar(adj2, 3, [3, 8, 3], 2)
   end
 end

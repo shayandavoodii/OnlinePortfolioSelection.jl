b8682cadcc68a7681bd97f644c1ba97e08dfe28d
Shayan <sh0davoodi@gmail.com>
2023-11-24 23:51:59 +0330

    [EGM] Exported 🚚

--- src/OnlinePortfolioSelection.jl
+++ src/OnlinePortfolioSelection.jl
@@ -34,17 +34,19 @@ include("Algos/CWMR.jl")
 include("Algos/CAEG.jl")
 include("Algos/OLDEM.jl")
 include("Algos/AICTR.jl")
+include("Algos/EGM.jl")
 include("Tools/metrics.jl")
 include("Tools/show.jl")
 include("Tools/tools.jl")
 include("Tools/cornfam.jl")
 
 export up, eg, cornu, cornk, dricornk, crp, bs, rprt, anticor, olmar, bk, load, mrvol, cwogd
-export uniform, cluslog, pamr, ppt, cwmr, caeg, oldem, aictr
+export uniform, cluslog, pamr, ppt, cwmr, caeg, oldem, aictr, egm
 export OPSMetrics, sn, mer, apy, ann_std, ann_sharpe, mdd, calmar
 export OPSAlgorithm, KMNLOG, KMDLOG, PAMR, PAMR1, PAMR2
 export CWMRD, CWMRS, Var, Stdev
 export SMA, EMA, PP
+export EGE, EGR, EGA
 export opsmethods
 
 @setup_workload begin
@@ -106,6 +108,7 @@ function opsmethods()
   println("      CAEG: Continuous Aggregating Exponential Gradient - Call `caeg`")
   println("     OLDEM: Online Low Dimension Ensemble Method - Call `oldem`")
   println("     AICTR: Adaptive Input and Composite Trend Representation - Call `aictr`")
+  println("       EGM: Exponential Gradient with Momentum - Call `egm`")
 end
 # COV_EXCL_STOP
 

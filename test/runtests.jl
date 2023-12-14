2afd89fb4c0f7c2a111fb7ed95974019fbc9dc97
Shayan <sh0davoodi@gmail.com>
2023-12-14 21:55:02 +0330

    [RMR} Unit tests provided 🧪

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,142 +4,66 @@ using Ipopt
 using Test
 using Statistics
 
-@testset "CORN.jl" begin
+@testset "Algorithms" begin
   @info "Run unit tests in CORN.jl"
   include("CORN.jl")
-end
-
-@testset "DRICORNK.jl" begin
   @info "Run unit tests in DRICORNK.jl"
   include("DRICORNK.jl")
+  @info "Run unit tests in UP.jl"
+  include("UP.jl")
+  @info "Run unit tests in RPRT.jl"
+  include("RPRT.jl")
+  @info "Run unit tests in EG.jl"
+  include("EG.jl")
+  @info "Run unit tests in CRP.jl"
+  include("CRP.jl")
+  @info "Run unit tests in BS.jl"
+  include("BS.jl")
+  @info "Run unit tests in Anticor.jl"
+  include("Anticor.jl")
+  @info "Run unit tests in OLMAR.jl"
+  include("OLMAR.jl")
+  @info "Run unit tests in BK.jl"
+  include("BK.jl")
+  @info "Run unit tests in LOAD.jl"
+  include("LOAD.jl")
+  @info "Run unit tests in MRvol.jl"
+  include("MRvol.jl")
+  @info "Run unit tests in CW-OGD.jl"
+  include("CW-OGD.jl")
+  @info "Run unit tests in uniform.jl"
+  include("uniform.jl")
+  @info "Run unit tests in CLUSLOG.jl"
+  include("CLUSLOG.jl")
+  @info "Run unit tests in PAMR.jl"
+  include("PAMR.jl")
+  @info "Run unit tests in PPT.jl"
+  include("PPT.jl")
+  @info "Run unit tests in CWMR.jl"
+  include("CWMR.jl")
+  @info "Run unit tests in CAEG.jl"
+  include("CAEG.jl")
+  @info "Run unit tests in IndivFuncs.jl"
+  include("IndivFuncs.jl")
+  @info "Run unit tests in OLDEM.jl"
+  include("OLDEM.jl")
+  @info "Run unit tests in AICTR.jl"
+  include("AICTR.jl")
+  @info "Run unit tests in EGM.jl"
+  include("EGM.jl")
+  @info "Run unit tests in TPPT.jl"
+  include("TPPT.jl")
+  @info "Run unit tests in GWR.jl"
+  include("GWR.jl")
+  @info "Run unit tests in ONS.jl"
+  include("ONS.jl")
+  @info "Run unit tests in DMR.jl"
+  include("DMR.jl")
+  @info "Run unit tests in RMR.jl"
+  include("RMR.jl")
 end
 
 @testset "metrics.jl" begin
   @info "Run unit tests in metrics.jl"
   include("metrics.jl")
-end
-
-@testset "UP.jl" begin
-  @info "Run unit tests in UP.jl"
-  include("UP.jl")
-end
-
-@testset "RPRT.jl" begin
-  @info "Run unit tests in RPRT.jl"
-  include("RPRT.jl")
-end
-
-@testset "EG.jl" begin
-  @info "Run unit tests in EG.jl"
-  include("EG.jl")
-end
-
-@testset "CRP.jl" begin
-  @info "Run unit tests in CRP.jl"
-  include("CRP.jl")
-end
-
-@testset "BS.jl" begin
-  @info "Run unit tests in BS.jl"
-  include("BS.jl")
-end
-
-@testset "Anticor.jl" begin
-  @info "Run unit tests in Anticor.jl"
-  include("Anticor.jl")
-end
-
-@testset "OLMAR.jl" begin
-  @info "Run unit tests in OLMAR.jl"
-  include("OLMAR.jl")
-end
-
-@testset "BK.jl" begin
-  @info "Run unit tests in BK.jl"
-  include("BK.jl")
-end
-
-@testset "LOAD.jl" begin
-  @info "Run unit tests in LOAD.jl"
-  include("LOAD.jl")
-end
-
-@testset "MRvol.jl" begin
-  @info "Run unit tests in MRvol.jl"
-  include("MRvol.jl")
-end
-
-@testset "CW-OGD.jl" begin
-  @info "Run unit tests in CW-OGD.jl"
-  include("CW-OGD.jl")
-end
-
-@testset "uniform.jl" begin
-  @info "Run unit tests in uniform.jl"
-  include("uniform.jl")
-end
-
-@testset "CLUSLOG.jl" begin
-  @info "Run unit tests in CLUSLOG.jl"
-  include("CLUSLOG.jl")
-end
-
-@testset "PAMR.jl" begin
-  @info "Run unit tests in PAMR.jl"
-  include("PAMR.jl")
-end
-
-@testset "PPT.jl" begin
-  @info "Run unit tests in PPT.jl"
-  include("PPT.jl")
-end
-
-@testset "CWMR.jl" begin
-  @info "Run unit tests in CWMR.jl"
-  include("CWMR.jl")
-end
-
-@testset "CAEG.jl" begin
-  @info "Run unit tests in CAEG.jl"
-  include("CAEG.jl")
-end
-
-@testset "IndivFuncs.jl" begin
-  @info "Run unit tests in IndivFuncs.jl"
-  include("IndivFuncs.jl")
-end
-
-@testset "OLDEM.jl" begin
-  @info "Run unit tests in OLDEM.jl"
-  include("OLDEM.jl")
-end
-
-@testset "AICTR.jl" begin
-  @info "Run unit tests in AICTR.jl"
-  include("AICTR.jl")
-end
-
-@testset "EGM.jl" begin
-  @info "Run unit tests in EGM.jl"
-  include("EGM.jl")
-end
-
-@testset "TPPT.jl" begin
-  @info "Run unit tests in TPPT.jl"
-  include("TPPT.jl")
-end
-
-@testset "GWR.jl" begin
-  @info "Run unit tests in GWR.jl"
-  include("GWR.jl")
-end
-
-@testset "ONS.jl" begin
-  @info "Run unit tests in ONS.jl"
-  include("ONS.jl")
-end
-
-@testset "DMR.jl" begin
-  @info "Run unit tests in DMR.jl"
-  include("DMR.jl")
 end

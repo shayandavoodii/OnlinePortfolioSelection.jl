2879dfa189bf2f73ba0fcde6ab4ecc9a6cbb4979
Shayan <sh0davoodi@gmail.com>
2024-02-09 13:22:22 +0330

    [metrics.jl] Minor docstring improvement ✨

--- src/Tools/metrics.jl
+++ src/Tools/metrics.jl
@@ -254,7 +254,8 @@ Calculate the Maximum Drawdown (MDD) of investment. Also, see [`sn`](@ref), [`me
 [`ann_std`](@ref), [`apy`](@ref), [`ann_sharpe`](@ref), [`calmar`](@ref), and [`opsmetrics`](@ref).
 
 # Arguments
-- `Sn::AbstractVector{T}`: the cumulative return of investment during the investment period.
+- `Sn::AbstractVector{T}`: the cumulative return of investment during the investment period. \
+  see [`sn`](@ref).
 
 # Returns
 - `::AbstractFloat`: the MDD of investment.

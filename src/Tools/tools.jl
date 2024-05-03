02ad8fb28ac1eff92fec9d9749de12cda55bc7fc
Shayan <sh0davoodi@gmail.com>
2024-05-03 22:11:54 +0330

    [tools.jl] `ttest` docstring updated 📄

--- src/Tools/tools.jl
+++ src/Tools/tools.jl
@@ -563,6 +563,13 @@ end
 
 """
     ttest(vec::AbstractVector{<:AbstractVector})
+    ttest(SB::AbstractVector, Sₜ::AbstractVector, SF::AbstractFloat)
+
+# Method 1
+
+```julia
+ttest(vec::AbstractVector{<:AbstractVector})
+```
 
 Perform a one sample t-test of the null hypothesis that `n` values with mean `x̄` and sample \
 standard deviation stddev come from a distribution with mean ``μ\\_0`` against the alternative \
@@ -573,14 +580,14 @@ Annual Percentage Yield (APY) of a different algorithm on various datasets.
 !!! note
     You have to install and import the `HypothesisTests` package to use this function.
 
-# Arguments
+## Arguments
 - `vec::AbstractVector{<:AbstractVector}`: A vector of vectors. Each inner vector should be \
 of the same size.
 
-# Returns
+## Returns
 - `::Matrix{<:AbstractFloat}`: A matrix of p-values for each pair of algorithms.
 
-# Example
+## Example
 ```julia
 julia> using OnlinePortfolioSelection, HypothesisTests
 
@@ -596,5 +603,26 @@ julia> ttest(apys)
  0.0  0.0  0.843672
  0.0  0.0  0.0
 ```
+
+# Method 2
+
+```julia
+ttest(SB::AbstractVector, Sₜ::AbstractVector, SF::AbstractFloat)
+```
+
+Performs a t-student test to check whether the returns gained by a trading algorithm is due \
+to a simple luck.
+
+!!! note
+    You have to install and import the `GLM` package to use this function.
+
+## Arguments
+- `SB::AbstractVector`: Denotes the daily returns of the benchmark (market index)
+- `Sₜ::AbstractVector`: Portfolio daily returns
+- `SF::AbstractFloat`: Daily returns of the risk-free assets (Can be set to Treasury bill \
+  value or annual interest rate.)
+
+- `::StatsModels.TableRegressionModel`: An object of type `TableRegressionModel` including \
+  the values of t-student test analysis.
 """
 function ttest end
